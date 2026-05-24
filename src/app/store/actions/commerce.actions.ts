22519252b08ed990eaca2a931633e9c187b84553
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-24 17:20:24 +0200

    Type commerce actions

--- src/app/store/actions/commerce.actions.ts
+++ src/app/store/actions/commerce.actions.ts
@@ -1,11 +1,15 @@
 import { createAction, props } from '@ngrx/store';
 
+import { CommerceItem } from '../../models/commerce-item.model';
+
 export const loadCommerces = createAction('[Commerce] Load Commerces');
+
 export const loadCommercesSuccess = createAction(
   '[Commerce] Load Commerces Success',
-  props<{ commerces: any[] }>()
+  props<{ commerces: CommerceItem[] }>()
 );
+
 export const loadCommercesFailure = createAction(
   '[Commerce] Load Commerces Failure',
-  props<{ error: any }>()
+  props<{ error: string }>()
 );
