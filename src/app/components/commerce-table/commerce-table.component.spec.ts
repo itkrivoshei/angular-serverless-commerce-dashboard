86f033d57fb421c547e483144b85e1285ef07845
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-24 17:31:32 +0200

    Fix commerce table component spec

--- src/app/components/commerce-table/commerce-table.component.spec.ts
+++ src/app/components/commerce-table/commerce-table.component.spec.ts
@@ -1,18 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from '../../app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
-describe('AppComponent', () => {
+import { CommerceTableComponent } from './commerce-table.component';
+import { commerceFeatureKey, initialState } from '../../store/reducers/commerce.reducer';
+
+describe('CommerceTableComponent', () => {
+  let component: CommerceTableComponent;
+  let fixture: ComponentFixture<CommerceTableComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [CommerceTableComponent],
+      providers: [
+        provideMockStore({
+          initialState: {
+            [commerceFeatureKey]: initialState,
+          },
+        }),
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(CommerceTableComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
   });
 });
