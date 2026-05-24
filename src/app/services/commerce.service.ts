8a64ff3c1616c55ae9d0867424c938aa65c45ce4
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-24 17:20:03 +0200

    Use shared commerce item model

--- src/app/services/commerce.service.ts
+++ src/app/services/commerce.service.ts
@@ -1,21 +1,16 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
-export interface CommerceItem {
-  product_name: string;
-  material: string;
-  department: string;
-  price: number;
-}
+import { environment } from '../../environments/environment';
+import { CommerceItem } from '../models/commerce-item.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommerceService {
-  private apiUrl = environment.apiUrl;
-  private limit = 20;
+  private readonly apiUrl = environment.apiUrl;
+  private readonly limit = 20;
   private skip = 0;
 
   constructor(private http: HttpClient) {}
