c715fa360b8ecff24d0d68314a52665a693feae1
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-24 17:20:54 +0200

    Track typed commerce loading state

--- src/app/store/reducers/commerce.reducer.ts
+++ src/app/store/reducers/commerce.reducer.ts
@@ -1,26 +1,37 @@
 import { createReducer, on } from '@ngrx/store';
+
+import { CommerceItem } from '../../models/commerce-item.model';
 import * as CommerceActions from '../actions/commerce.actions';
 
 export const commerceFeatureKey = 'commerce';
 
 export interface State {
-  commerces: any[];
-  error: any;
+  commerces: CommerceItem[];
+  isLoading: boolean;
+  error: string | null;
 }
 
 export const initialState: State = {
   commerces: [],
-  error: undefined,
+  isLoading: false,
+  error: null,
 };
 
 export const reducer = createReducer(
   initialState,
+  on(CommerceActions.loadCommerces, (state) => ({
+    ...state,
+    isLoading: true,
+    error: null,
+  })),
   on(CommerceActions.loadCommercesSuccess, (state, action) => ({
     ...state,
     commerces: [...state.commerces, ...action.commerces],
+    isLoading: false,
   })),
   on(CommerceActions.loadCommercesFailure, (state, action) => ({
     ...state,
+    isLoading: false,
     error: action.error,
   }))
 );
